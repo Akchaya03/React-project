6a8011db2e6f75be0715dd6b9a11675db75b45b3
Akchaya03 <114875073+Akchaya03@users.noreply.github.com>
2023-07-31 16:37:30 +0530

    Add files via upload

---
+++ task3/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Task3 from './task3/task3';
+ReactDOM.render(<Task3/>,document.getElementById('root'));
+
+
+
+//import Registrationform from './project5';
+//ReactDOM.render(<Registrationform/>,document.getElementById('root'));
